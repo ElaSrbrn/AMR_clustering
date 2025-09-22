b17254a8de5597d0ee6e87b2ebce0e3e3e7fa20e
Sam-Tuna <66344030+Sam-Tuna@users.noreply.github.com>
2025-09-21 23:10:08 -0300

    corrected basecalling script
    Fixed model selection; removed unnecessary --verbose flag; fixed output file writing; removed dorado trimming call since the reads are automatically trimmed at the demultiplexing step; removed file conversion from bam to fastq to be added on a cpu based script downstream; rewrote the script to run on relative paths of the work environment, as opposed to absolute paths, but kept the option to add absolute paths in case they're needed

--- basecalling_new.sh
+++ basecalling_new.sh
@@ -1,55 +1,26 @@
 #!/bin/bash
 #SBATCH -p gpu_p
-#SBATCH -q gpu_long
-#SBATCH --mem=250G
-#SBATCH -t 72:00:00
-#SBATCH --nice=10000
-#SBATCH --job-name=dorado_nanoplot
-#SBATCH -c 12
-# Set working directory to dorado v1.1.1 binary location
-cd /home/haicu/ela.sauerborn/dorado-1.1.1-linux-x64/bin
+#SBATCH -q gpu_normal
+#SBATCH --mem=128G
+#SBATCH -t 24:00:00
+#SBATCH --mail-user=samir.vargas@helmholtz-munich.de
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=dorado
+#SBATCH --cpus-per-task=8
 
-# === Input / Output Paths ===
-READS_DIR="/lustre/groups/hpc/urban_lab/backup/plasmid_project/work_package01/CSF_2025/96Pool/20250226_1401_P2S-01622-B_PAY37630_aa601ade/pod5_skip"
-OUTDIR="/lustre/groups/hpc/urban_lab/projects/plasmid_project/stored_resistant_enterobacterales/work_package01/26022025/basecalled_v5_6mA4mC5mC"
+
+READS_DIR="pod5"
+OUTDIR="basecalled_v5allmods"
 DEMUX="${OUTDIR}/demultiplexed"
-TRIMDIR="${OUTDIR}/trimmed"
-mkdir -p "$OUTDIR" "$DEMUX" "$TRIMDIR"
-
-# === Models ===
-BASE_MODEL="dna_r10.4.1_e8.2_400bps_sup@v5.0.0"
-MODBASE_MODELS="dna_r10.4.1_e8.2_400bps_sup@v5.0.0_4mC_5mC@v3,dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3"
+DORADO_BIN="/home/haicu/ela.sauerborn/dorado-1.1.1-linux-x64/bin/dorado"
+mkdir -p "$DEMUX" #"$TRIMDIR"
+# BASE_MODEL="dna_r10.4.1_e8.2_400bps_sup@v5.0.0"
+# ODBASE_MODELS="dna_r10.4.1_e8.2_400bps_sup@v5.0.0_4mC_5mC@v3,dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3"
 KIT="SQK-RBK114-96"
 
-# === Step 1: Basecalling with modbase support ===
-./dorado basecaller "$BASE_MODEL" "$READS_DIR" \
-  --kit-name "$KIT" \
-  --modified-bases-models "$MODBASE_MODELS" \
-  --output "$OUTDIR/all.bam" \
-  --verbose
+#basecalling
+${DORADO_BIN} basecaller sup@v5.0.0,4mC_5mC,6mA $READS_DIR --kit-name $KIT --no-trim > "$OUTDIR/all.bam"
 
-# === Step 2: Demultiplexing ===
-./dorado demux --kit-name "$KIT" \
-  --output-dir "$DEMUX" \
-  "$OUTDIR/all.bam"
-
-# === Step 3: Trimming ===
-for bam in "$DEMUX"/*.bam; do
-    sample="$(basename "$bam" .bam)"
-    mkdir -p "${TRIMDIR}/${sample}"
-    ./dorado trim "$bam" --kit-name "$KIT" > "${TRIMDIR}/${sample}/trimmed.bam"
-done
-
-
-
-eval "$(micromamba shell hook --shell=bash)"; 
-micromamba activate assembly
-
-mkdir -p "$OUTDIR/demux_fastq"
-for b in "$OUTDIR"/demux_bam/*.bam; do
-  base=$(basename "${b%.bam}")
-  samtools fastq "$b" | gzip > "$OUTDIR/demux_fastq/${base}.fastq.gz"
-done
-
-
-
+#demultiplexing
+${DORADO_BIN} demux --kit-name "$KIT" --output-dir "$DEMUX" "$OUTDIR/all.bam"
