418f0d73e5ea32983a08ee7019e5968d93fdc8dc
ElaSrbrn <150910191+ElaSrbrn@users.noreply.github.com>
2026-05-07 22:41:38 +0200

    Update 01_convert_bam_tofastq.sh

--- 01_convert_bam_tofastq.sh
+++ 01_convert_bam_tofastq.sh
@@ -9,25 +9,15 @@ FASTQ_DIR=${FASTQ_DIR:-../fastq}
 MIN_BARCODE=${MIN_BARCODE:-1}
 MAX_BARCODE=${MAX_BARCODE:-24}
 
-mkdir -p "$FASTQ_DIR"
-
-# Convert BAM → FASTQ.gz
 for bam in "$INDIR"/*_barcode*.bam; do
-  barcode=$(basename "$bam" .bam | grep -oE 'barcode[0-9]{2}')
-  [[ -z "$barcode" ]] && continue
-  num=${barcode#barcode}
+  base=${bam##*/}
+  base=${base%.bam}
 
-  # numeric check (handle leading zeros correctly)
-  if (( 10#$num < MIN_BARCODE || 10#$num > MAX_BARCODE )); then
-    continue
-  fi
+  [[ $base =~ barcode([0-9]{2}) ]] || continue
+  num=$((10#${BASH_REMATCH[1]}))
 
-  base="$(basename "$bam" .bam)"
+  (( num < MIN_BARCODE || num > MAX_BARCODE )) && continue
+
   out="$FASTQ_DIR/${base}.fastq.gz"
-  [[ -s "$out" ]] && continue
-
-  echo "Converting $bam → $out"
-  samtools fastq -@ "$THREADS" -F 0x900 "$bam" | gzip > "$out"
+  [[ -s $out ]] || samtools fastq -@ "$THREADS" -F 0x900 "$bam" | gzip > "$out"
 done
-
-#only use primary mapped reads
